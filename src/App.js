24e69712bfbe10ee7b55db6843e9165729964f08
libertron <marcusserginio96@gmail.com>
2019-07-08 11:44:42 -0400

    react folder got clean and reset at initial point

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ColoredBlock from  './ColoredBlock.js';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p className="App-intro">
-            Hi {this.props.name}!
-          </p>
-          <ColoredBlock />
-        </header>
+        
       </div>
     );
   }
